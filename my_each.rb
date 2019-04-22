808cba2943911f24feb1b19e1e129c3ace4badf5
feven <feventsegay6@gmail.com>
2019-04-22 15:13:14 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+ 
+ i=0
+ while i < collection.length 
+ yield(collection[i])
+ i+=1
+ end 
+ collection
 end
+
+my_each(collection) {|x| puts x}
+
